7cbd33b54e5f81b47bd54b8d47e1c91de0135008
Sergey <smr.kolesnikov@gmail.com>
2022-10-13 19:58:28 +0500

    lesson_4 News model

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,1 +1,29 @@
-# Create your models here.
+from email.policy import default
+from tabnanny import verbose
+from django.db import models
+
+class News(models.Models):
+    title = models.CharField(max_length=256, verbose_name="Title")
+    preambule = models.CharField(max_length=1024, verbose_name="Preambule")
+    body = models.TextField(blank=True, null=True, verbose_name="Body")
+    body_as_markdown = models.BooleanField(
+        default=False, verbose_name="As markdown"
+    )
+    created = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Created",
+        editable=False
+    )
+    update = models.DateTimeField(
+        auto_now=True,
+        verbose_name="Edited",
+        editable=False
+    )
+    deleted = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return f"{self.pk} {self.title}"
+
+    def delete(self, *args):
+        self.delete = True
+        self.save()
