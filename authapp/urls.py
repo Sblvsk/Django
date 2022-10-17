ef9e11b920a1a4c6d4663fa24d2fd703e6858d28
Sergey <smr.kolesnikov@gmail.com>
2022-10-17 18:13:06 +0500

    lesson_6 News CRUD and permissions

--- authapp/urls.py
+++ authapp/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path("login/", views.CustomLoginView.as_view(), name="login"),
     path("logout/", views.CustomLogoutView.as_view(), name="logout"),
     path("register/", views.RegisterView.as_view(), name="register"),
-    path("profile_edit/<int:pk>/",
+    path(
+        "profile_edit/<int:pk>/",
         views.ProfileEditView.as_view(),
         name="profile_edit",
     ),
